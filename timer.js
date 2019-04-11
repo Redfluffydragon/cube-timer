1e63e44ba9f70d63d2c3b1bd34e28a3e593ccd43
Redfluffydragon <kderu1420@gmail.com>
2019-04-11 16:50:09 -0600

    More scramble button positioning for mobile

--- timer.js
+++ timer.js
@@ -326,7 +326,7 @@ function onStart() {
   scrambletxt.classList.add("transOneSec");
   if (isMobile) {
     let timeHeight = time.offsetTop;
-    multiScram.style.bottom = (window.innerHeight - timeHeight) + 'px';
+    multiScram.style.bottom = (window.innerHeight - timeHeight - 20) + 'px';
   }
 };
 
